36af265f1320e7d00c6979215b3fde19bc535aa0
Mapato2000 <67655321+Mapato2000@users.noreply.github.com>
2021-01-14 17:19:44 +0100

    Update Arbitre.java

--- Arbitre.java
+++ Arbitre.java
@@ -1,5 +1,18 @@
 public class Arbitre extends Persona{
-    public void excloure(Instruccio instr){
-
-    }
+	
+	Partit partit;
+	List<Instruccio> instruccions;
+	
+	public Arbitre(Partit part) {
+		
+		partit=part;
+		instruccions = new ArrayList<Instruccio>();
+	}
+	
+	public void excloure(Instruccio instr){
+		
+		partit.excloureJugador(instr);
+		instruccions.add(instr);
+	}
+	
 }
