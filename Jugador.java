031c22e2af6791c9376f0d61cbbe359e29ad240b
Arnau Pagès <62243257+arnaupages7@users.noreply.github.com>
2021-01-14 20:05:45 +0100

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,4 +1,6 @@
 import java.util.*; 
+import java.io.*;
+
 public class Jugador extends Persona{
     private String estil;
     private double alcada;
@@ -7,16 +9,56 @@ public class Jugador extends Persona{
     private double velocitat;
     private double orientacio;
     private double acceleracio;
-    private boolean amonestat;
+    private int amonestat;
     private Rol rol;
     private Entrenador entrenador;
-    private int exclusions;
+    private int nExclusions;
     private boolean veuPilota;
     private boolean pendentEntrenador;
     private boolean pendentArbitre;
     private boolean banqueta;
     private Vector<Instruccio> msgEntrenador;
 
+    public void rebreMissatge (Instruccio instruccio) {
 
+        msgEntrenador.add(instruccio);
+        System.out.println("Missatge rebut de l'entreandor: " + instruccio.missatge() + "\n");
+
+    }
+
+    public int excloure() {
+
+        exclusions++;
+        return exclusions;
+
+    }
+
+    public int exclosions () {
+        return exclusions;
+    }
+
+    public void enviarPista () {
+
+        banqueta = false;
+        pendentArbitre = true;
+        pendentEntrenador = true;
+        veuPilota = true;
+
+    }
+    
+    public void enviarBanqueta () {
+
+        banqueta = true;
+        pendentArbitre = false;
+        pendentEntrenador = true;
+        veuPilota = true;
+
+    }
+
+    public void canviarRol (Instruccio instruccio) {
+
+        rol = instruccio.nouRol();
+
+    }
 
 }
